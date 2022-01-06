b1c091684580e8f73fa658f94d29ee668ae0d5e3
purmonth <purmonth@gmail.com>
2022-01-06 11:14:04 +0800

    fix: two_sum

--- easy/1.Two_Sum.cpp
+++ easy/1.Two_Sum.cpp
@@ -9,28 +9,22 @@ public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
 
-        map<int, int> dict;
-        map<int, int>::iterator iter;
-        vector<int> ans;
-
-        for (int i = 0; i < nums.size(); i++)
+        vector<int> ret;
+        int size = nums.size();
+        int diff;
+        map<int, int> m;
+        for (int i = 0; i < size; i++)
         {
-
-            iter = dict.find(target - nums[i]);
-            // cout << iter->first << " " << iter->second;
-            //  printf("(%d,%d)", iter->first, iter->second);
-            if (iter != dict.end())
-            {                                // num found
-                ans.push_back(iter->second); // nums find in dict (smaller)
-                ans.push_back(i);            // current index (bigger)
-                return ans;
+            diff = target - nums[i];
+            if (m.find(diff) != m.end() && m.find(diff)->second != i)
+            {
+                ret.push_back(i);
+                ret.push_back(m.find(diff)->second);
+                return ret;
             }
-            else
-            { // iter == dict.end(), not found
-                dict[nums[i]] = i;
-            }
+            m[nums[i]] = i;
         }
-        return ans;
+        return ret;
     }
 };
 
@@ -40,16 +34,6 @@ int main()
     int a[4] = {2, 7, 11, 15};
     int target = 9;
     vector<int> v(a, a + 4);
-
-    // 1. 使用足標運算子 function member - at
-    for (int i = 0; i < v.size(); i++)
-        cout << v[i] << " ";
-    cout << endl;
-    for (int i = 0; i < v.size(); i++)
-        cout << v.at(i) << " ";
-    cout << endl;
-
-    // 2. 使用 iterator
     vector<int>::iterator it_i;
     for (it_i = v.begin(); it_i != v.end(); ++it_i)
         cout << *it_i << " ";
@@ -57,6 +41,6 @@ int main()
 
     for (int i = 0; i < sol.twoSum(v, target).size(); i++)
         cout << sol.twoSum(v, target)[i] << " ";
-        
+
     return 0;
 }
