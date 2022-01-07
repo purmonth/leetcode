0086f122d7206bc49d54e263e1fe1a6ba1145486
purmonth <purmonth@gmail.com>
2022-01-07 15:17:26 +0800

    feat: Longest_Common_Prefix

---
+++ easy/14.Longest_Common_Prefix.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        if (strs.empty())
+            return "";
+        if (strs.size() == 1)
+            return strs[0];
+        string ans = "";
+        int min_len = 200;
+
+        for (int i = 0; i < strs.size(); i++)
+            if (strs[i].size() < min_len)
+                min_len = strs[i].size();
+
+        for (int i = 0; i < min_len; i++)
+        {
+            for (int j = 0; j < strs.size()-1; j++)
+            {
+                if (strs[j][i] != strs[j + 1][i])
+                {
+                    return ans;
+                }
+            }
+            ans += strs[0][i];
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    string strArr[] = {"flower", "flow", "flight"};
+    vector<string> input(strArr, strArr + 3);
+
+    cout << sol.longestCommonPrefix(input);
+    return 0;
+}
