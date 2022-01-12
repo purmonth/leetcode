bccee49fda805cacfbfa9ff1d81aab47d2322ec8
purmonth <purmonth@gmail.com>
2022-01-13 01:05:31 +0800

    feat: remove nth node from end of list

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -1,147 +1,87 @@
-class MyLinkedList {
-    struct Node{
-        int data;
-        Node* next;
-        
-        Node(int val){
-            data = val;
-            next = NULL;
-        }
-    };
-    
-    Node *head;
-    int size;
-    
-    void printList(){
-        auto itt = head;
-        while(itt != NULL){
-            cout << itt->data << " ";
-            itt = itt->next;
-        }
-        cout << endl;
-    }
-public:
-    /** Initialize your data structure here. */
-    MyLinkedList() {
-        head = NULL;
-        size = 0;
-    }
-    
-    MyLinkedList(int val) {
-        Node* node = new Node(val);
-        head = node;
-        size = 1;
-    }
-    
-    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
-    int get(int index) {
-        auto itt = head;
-        if(index > size)   return -1;
-        int currIdx = 0;
-        while(itt != NULL){
-            if(index == currIdx)    return itt->data;
-            currIdx += 1;
-            itt = itt->next;
-        }
-        return -1;
-    }
-    
-    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
-    void addAtHead(int val) {
-        Node* node = new Node(val);
-        
-        if(head == NULL){
-            head = node;
-            size++;            
-            return;
-        }
-                
-        node->next = head;
-        head = node;
-        size++;
-    }
-    
-    /** Append a node of value val to the last element of the linked list. */
-    void addAtTail(int val) {
-        Node* node = new Node(val);
-        
-        if(head == NULL){
-            head = node;
-            size++;
-            return;
-        }
-        
-        auto itt = head;
-        while(itt->next != NULL){
-            itt = itt->next;
-        }
-        
-        itt->next = node;
-        size++;
-    }
-    
-    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
-    void addAtIndex(int index, int val) {
-        if(index > size)   return;
-        if(index == 0)  {
-            addAtHead(val);
-            return;
-        }
-        
-        auto itt = head;
-        Node* node = new Node(val);
-        int currIdx = 1;
-        
-        while(itt != NULL && currIdx != index){
-            currIdx++;
-            itt = itt->next;
-        }
-        
-        node->next = itt->next;
-        itt->next = node;
-        size++;        
-    }
-    
-    /** Delete the index-th node in the linked list, if the index is valid. */
-    void deleteAtIndex(int index) {
+#include <iostream>
+using namespace std;
 
-        if(index >= size)   return;
-        if(index == 0){
-            auto temp = head;
-            head = head->next;
-            delete head;
-            size--;
-            return;
-        }
-        
-        auto itt = head;
-        int currIdx = 1;
-        
-        while(itt->next != NULL){
-            if(index == currIdx){
-                auto temp = itt->next;
-                itt->next = itt->next->next;
-                size--;
-                delete temp;
-                return;
-            }
-            currIdx++;
-            itt = itt->next;
-        }
-    }
-};
+struct node {
+  int num;
+  node *nextptr;
+} * stnode;  // node constructed
 
+void make(int n);
+void printMiddle(struct node *stnode);
+void print();
 
-int main(){
-    
-    return 0;
+int main()  // Main method
+{
+  int n, num;
+
+  cout << "Enter the number of nodes: ";
+  cin >> n;
+  make(n);
+  cout << "\nLinked list data: \n";
+  print();
+  printMiddle(stnode);
+
+  return 0;
 }
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
+void make(int n)  // function to create linked list.
+{
+  struct node *frntNode, *tmp;
+  int num, i;
+
+  stnode = (struct node *)malloc(sizeof(struct node));
+  if (stnode == NULL) {
+    cout << "Memory can not be allocated";
+  } else {
+    cout << "Enter the data for node 1: ";
+    cin >> num;
+    stnode->num = num;
+    stnode->nextptr = NULL;  // Links the address field to NULL
+    tmp = stnode;
+
+    for (i = 2; i <= n; i++) {
+      frntNode = (struct node *)malloc(sizeof(struct node));
+
+      if (frntNode == NULL)  // If frntnode is null no memory cannot be allotted
+      {
+        cout << "Memory can not be allocated";
+        break;
+      } else {
+        cout << "Enter the data for node " << i
+             << ": ";  // Entering data in nodes.
+        cin >> num;
+        frntNode->num = num;
+        frntNode->nextptr = NULL;
+        tmp->nextptr = frntNode;
+        tmp = tmp->nextptr;
+      }
+    }
+  }
+}
+void print()  // method to display linked list
+{
+  struct node *tmp;
+  if (stnode == NULL) {
+    cout << "List is empty";
+  } else {
+    tmp = stnode;
+    cout << "Linked List\t";
+    while (tmp != NULL) {
+      cout << tmp->num << "\t";
+      tmp = tmp->nextptr;
+    }
+  }
+}
+void printMiddle(
+    struct node *stnode)  // function to print middle node of the list.
+{
+  struct node *single_ptr = stnode;
+  struct node *twice_ptr = stnode;
+
+  if (stnode != NULL) {
+    while (twice_ptr != NULL && twice_ptr->nextptr != NULL) {
+      twice_ptr = twice_ptr->nextptr->nextptr;  // moving with twice speed.
+      single_ptr = single_ptr->nextptr;
+    }
+    cout << "The middle element is " << single_ptr->num;
+  }
+}
