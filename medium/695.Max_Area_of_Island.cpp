7d98d0435c1a468668be6a5bc2ce07df4e75355b
purmonth <purmonth@gmail.com>
2022-01-15 01:26:58 +0800

    feat: max area of island

---
+++ medium/695.Max_Area_of_Island.cpp
@@ -0,0 +1,60 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+  int findPath(vector<vector<int>>& grid, int x, int y) {
+    int counter = 0;
+    if (grid[x][y] == 1) {
+      counter++;
+      grid[x][y] = 0;
+      if (grid[x + 1][y] == 1) {
+        counter += findPath(grid, x + 1, y);
+      }
+      if (grid[x - 1][y] == 1) {
+        counter += findPath(grid, x - 1, y);
+      }
+      if (grid[x][y + 1] == 1) {
+        counter += findPath(grid, x, y + 1);
+      }
+      if (grid[x][y - 1] == 1) {
+        counter += findPath(grid, x, y - 1);
+      }
+    }
+    return counter;
+  }
+
+ public:
+  int maxAreaOfIsland(vector<vector<int>>& grid) {
+    int sum = 0;
+    vector<vector<int>> big_grid(grid.size() + 2,
+                                 vector<int>(grid[0].size() + 2, 0));
+    for (int i = 0; i < grid.size(); i++) {
+      for (int j = 0; j < grid[0].size(); j++) {
+        big_grid[i + 1][j + 1] = grid[i][j];
+      }
+    }
+    for (int i = 0; i < big_grid.size(); i++) {
+      for (int j = 0; j < big_grid[0].size(); j++) {
+        sum = max(findPath(big_grid, i, j), sum);
+      }
+    }
+    return sum;
+  }
+};
+
+int main() {
+  Solution sol;
+  vector<vector<int>> input{{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                            {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                            {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                            {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+  int result = sol.maxAreaOfIsland(input);
+  cout << result << endl;
+  return 0;
+}
---
+++ template/2darray_template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+ public:
+};
+
+int main() {
+  Solution sol;
+  vector<vector<int>> input{{}};
+  result = sol;
+  cout << (result ? "true" : "false") << endl;
+  return 0;
+}
