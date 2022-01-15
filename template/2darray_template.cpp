b4ec0d45873d2206f06eaf61434ccd9f764b71c3
purmonth <purmonth@gmail.com>
2022-01-15 14:09:31 +0800

    feat: flood fill

--- template/2darray_template.cpp
+++ template/2darray_template.cpp
@@ -5,10 +5,18 @@ class Solution {
  public:
 };
 
+void traverse(vector<vector<int>> input) {
+  for (int i = 0; i < input.size(); i++) {
+    for (int j = 0; j < input[0].size(); j++) {
+      cout << input[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
 int main() {
   Solution sol;
   vector<vector<int>> input{{}};
-  result = sol;
-  cout << (result ? "true" : "false") << endl;
+  traverse(input);
   return 0;
 }
